37eeac81ea0da27b60643749a11651b37a4e3fc6
GustavoHMB345 <gustavohx3bozz@gmail.com>
2025-10-16 11:14:04 -0300

    Improve project structure and remove unnecessary files

--- e2e/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testTimeout: 120000,
-  testRunner: 'jest-circus/runner',
-  reporters: ['detox/runners/jest/streamlineReporter'],
-  setupFilesAfterEnv: ['detox/runners/jest/setup'],
-  testMatch: ['**/?(*.)+(e2e).[jt]s?(x)'],
-};
--- eslint.config.js
+++ eslint.config.js
@@ -10,38 +10,5 @@ module.exports = defineConfig([
   {
     ignores: ['dist/*'],
   },
-  // Jest test files: define globals so lint doesn't error on jest/describe/it/expect
-  {
-    files: ['**/__tests__/**', '**/*.test.*', 'jest.setup.js'],
-    languageOptions: {
-      globals: {
-        jest: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        expect: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-      },
-    },
-  },
-  // Detox e2e tests
-  {
-    files: ['e2e/**/*.e2e.*', 'e2e/**/*.test.*', 'e2e/**'],
-    languageOptions: {
-      globals: {
-        device: 'readonly',
-        element: 'readonly',
-        by: 'readonly',
-        expect: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-      },
-    },
-  },
+  // Test-specific configs removed
 ]);
